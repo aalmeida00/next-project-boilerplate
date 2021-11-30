2824b3630b0bdcad41d5684189f34c2c86fbeb45
Andre Almeida <andre-silva78@hotmail.com>
2021-11-30 00:03:11 -0300

    Add Styled Components and create GlobalStyles structure

---
+++ src/pages/_app.tsx
@@ -0,0 +1,24 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyle from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Next.js + TypeScript</title>
+        <link rel="shortcut icon" href="" />
+        <link rel="apple-touch-icon" href="" />
+        <meta
+          name="description"
+          content="a simple boilerplate to use TS + Next"
+        />
+      </Head>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  )
+}
+
+export default App
