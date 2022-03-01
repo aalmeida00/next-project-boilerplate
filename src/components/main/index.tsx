7068cf6f57882e95d27cc3272490dddf6cb38044
Andre Almeida <andre-silva78@hotmail.com>
2022-03-01 20:20:25 -0300

    Add first styled components style and testing component

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 
+import * as S from './styles'
+
 const Main = () => {
   return (
-    <main>
-      <h2>React Avançado</h2>
-    </main>
+    <S.Wrapper>
+      <S.Logo src="/img/logo.svg" alt="React Avançado Logo" />
+      <S.Title>React Avançado</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS, Styled Components.
+      </S.Description>
+      <S.Illustration src="/img/hero.svg" alt="Um astronauta na lua" />
+    </S.Wrapper>
   )
 }
 
